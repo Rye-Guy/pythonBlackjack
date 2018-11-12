6e9ff28f889c445ccb12a778ba10d9589e12b04b
Rye-Guy <ryanracioppo95@gmail.com>
2018-11-12 16:53:17 -0500

    added main functions

--- main.py
+++ main.py
@@ -56,7 +56,85 @@ class Hand:
             self.value -= 10 
             self.aces -= 1
 
+class Chips:
 
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+
+def take_bet(chips):
+    while True: 
+        try: 
+            chips.bet = int(input('How many chips would you like to bet?'))
+        except:
+            print('Sorry, you must enter a vaild number to bet')
+        else:
+            if chips.bet > chips.total:
+                print('You are betting more than your total amount of chips! Total Chips: ' + chips.total)
+            else:
+                break
+
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck, hand):
+    global playing
+    while True:
+        
+        x = input('Would you like to Hit or Stand? Enter "h" or "s')
+
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        elif x[0].lower() == 's':
+            print('You stand. It is now the dealers turn!')
+            playing = False
+        else:
+            print('Please play again')
+            continue
+        break 
+
+def show_some(player, dealer):
+    print("/nDealer's Hand")
+    print("HIDDEN CARD")
+    print('', dealer.cards[1])
+    print("/nPlayer's Hand:", *player.cards, sep='/n ')
+
+def show_all(player, dealer):
+    print("/nDealer's Hand", *dealer.cards, sep='/n ')
+    print("Dealer's Hand =", dealer.value)
+    print("/nPlayer's Hand:", *player.cards, sep='/n ')
+    print("Player's Hand = ", player.value)
+
+
+def player_busts(player,dealer,chips):
+    print('You Busted!')
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print('You Win!')
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print('Dealer Bust!')
+    chips.win_bet()
+
+def dealer_wins(player,dealer,chips):
+    print('Dealer Wins!')
+    chips.lose_bet()
+
+def push(player, dealer):
+    print("Dealer and Player tie! It's a push.") 
 
 test_deck = Deck()
 test_deck.shuffle()
